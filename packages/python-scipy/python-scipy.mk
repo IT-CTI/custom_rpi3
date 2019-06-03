05a48f474d303b10c523e2cc56d960f56b3d3535
Eugene Batogov <e.batogov@cti.ru>
2019-06-03 17:41:35 +0300

    add scipy

--- packages/python-scipy/python-scipy.mk
+++ packages/python-scipy/python-scipy.mk
@@ -10,7 +10,9 @@ PYTHON_SCIPY_SITE = https://github.com/scipy/scipy/releases/download/v$(PYTHON_S
 PYTHON_SCIPY_LICENSE = BSD-3-Clause, BSD-2-Clause, PSF, Apache-2.0, MIT, Zlib
 PYTHON_SCIPY_LICENSE_FILES = LICENSE.txt
 PYTHON_SCIPY_SETUP_TYPE = setuptools
-PYTHON_SCIPY_DEPENDENCIES = host-python-numpyf
+PYTHON_SCIPY_ENV= LD_PRELOAD="/home/johnbat26/kingston/projects/IoT/custom_rpi3/.nerves/artifacts/custom_rpi3-portable-1.7.2/host/lib/libgfortran.so.4" 
+PYTHON_SCIPY_BUILD_OPTS= --fcompiler=gnu95
+PYTHON_SCIPY_DEPENDENCIES = host-python-numpy
 
 $(eval $(python-package))
-# $(eval $(host-python-package))
+$(eval $(host-python-package))
